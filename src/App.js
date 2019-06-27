894bb0b930fd0579d24863a28290c0d81384d0c8
Nishi Lad <nishilad2208@gmail.com>
2019-06-27 17:15:22 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -30,10 +30,10 @@ const App = () => {
     return (
         <div className="App">
             <h1>{cnt.counter}</h1>
-            <button onClick={increment}>Increment</button>
-            <button onClick={decrement}>Decrement</button>
-            <button onClick={() => addition(5)}>Addition</button>
-            <button onClick={() => subtraction(5)}>Subtraction</button>
+            <button onClick={increment} className="btn-primary mx-2">Increment</button>
+            <button onClick={decrement} className="btn-primary mx-2">Decrement</button>
+            <button onClick={() => addition(5)} className="btn-primary mx-2">Addition</button>
+            <button onClick={() => subtraction(5)} className="btn-primary mx-2">Subtraction</button>
         </div>
     )
 
